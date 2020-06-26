0f9065de1b81a8d462bc5c55198e9b979ace5741
Mikey <31766607+andmikey@users.noreply.github.com>
2020-06-26 20:55:35 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Knee-point and knee-onset in lithium-ion cells.',
+    author='Mikey',
+    license='',
+)
